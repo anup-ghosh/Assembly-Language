c10c060da8d8bd95e9ad520c2ec93687e950d145
Fagun Molla <70126358+Fagun06@users.noreply.github.com>
2023-11-08 21:46:56 +0600

    Create print 0-9.asm

---
+++ print 0-9.asm
@@ -0,0 +1,40 @@
+.model small
+.stack 
+.data
+m db 'Loop concept$'
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    
+    mov ah,9
+   lea dx,m
+    int 21h
+    
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    
+    
+    mov cx,10
+    mov ah,2
+    mov dl,'1'
+    
+    
+    level1:
+    int 21h
+    
+    inc dl
+    loop level1
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
